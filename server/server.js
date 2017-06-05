df907044a1b1d0e83a2d1f6c12901d6656d90d54
unknown <huhsy@swhitech.com>
2017-06-05 13:41:03 +0900

    Test the DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -6,6 +6,8 @@ const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
 
+const port = process.env.PORT || 3000;
+
 var app = express();
 
 app.use(bodyParser.json());
@@ -49,8 +51,26 @@ app.get('/todos/:id', (req, res) => {
 	});
 });
 
-app.listen(3000, () => {
-	console.log('Started on port 3000');
+app.delete('/todos/:id', (req, res) => {
+	var id = req.params.id;
+
+	if(!ObjectID.isValid(id)) {
+		return res.status(404).send();
+	}
+
+	Todo.findByIdAndRemove(id).then((todo) => {
+		if(!todo) {
+			return res.status(404).send();
+		}
+
+		res.send({todo});
+	}, (e) => {
+		res.status(400).send();
+	});
+});
+
+app.listen(port, () => {
+	console.log(`Started up at port ${port}`);
 });
 
 module.exports = {app};
